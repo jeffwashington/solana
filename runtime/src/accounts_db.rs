fd9dd7bdaaef003d5722b5253609b948665c0495
Jeff Washington (jwash) <wash678@gmail.com>
2021-01-27 13:46:43 -0600

    remove warn

--- runtime/src/accounts_db.rs
+++ runtime/src/accounts_db.rs
@@ -3956,9 +3956,6 @@ impl AccountsDB {
             .get_slot_stores(slot)
             .map(|res| res.read().unwrap().values().cloned().collect())
             .unwrap_or_default();
-        if storage_maps.len() > 10000 {
-            warn!("get_storage_maps, found slots: {}", storage_maps.len());
-        }
         storage_maps
     }
 
