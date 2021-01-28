fa5c5a40956e10224867f6b1815b1419e131fb57
Jeff Washington (jwash) <wash678@gmail.com>
2021-01-28 17:32:57 -0600

    check more often

--- runtime/src/accounts_db.rs
+++ runtime/src/accounts_db.rs
@@ -4268,6 +4268,15 @@ impl AccountsDB {
         } else {
             let r = self.calculate_accounts_hash(slot, ancestors, true, simple_capitalization_enabled)
                 .unwrap();
+
+            let s = self.get_snapshot_storages(slot);
+            Self::calculate_accounts_hash_using_stores_only(
+                s,
+                simple_capitalization_enabled,
+                r.2,
+                r.0,
+            );
+        
             (r.0, r.1)
         }
     }
