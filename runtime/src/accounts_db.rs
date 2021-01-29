80128927aaffacfd819e49f395a9a4689aa2bf24
Jeff Washington (jwash) <wash678@gmail.com>
2021-01-29 10:15:35 -0600

    sort by slot, then version

--- runtime/src/accounts_db.rs
+++ runtime/src/accounts_db.rs
@@ -165,7 +165,7 @@ type ReclaimResult = (AccountSlots, AppendVecOffsets);
 type StorageFinder<'a> = Box<dyn Fn(Slot, usize) -> Arc<AccountStorageEntry> + 'a>;
 type ShrinkCandidates = HashMap<Slot, HashMap<AppendVecId, Arc<AccountStorageEntry>>>;
 
-type CalculateHashIntermediate = (u64, Hash, u64, u64);
+type CalculateHashIntermediate = (u64, Hash, u64, u64, Slot);
 
 trait Versioned {
     fn version(&self) -> u64;
@@ -3927,7 +3927,7 @@ impl AccountsDB {
         scan_func: F,
     ) -> Vec<B>
     where
-        F: Fn(LoadedAccount, AppendVecId, &mut B) + Send + Sync,
+        F: Fn(LoadedAccount, AppendVecId, &mut B, Slot) + Send + Sync,
         B: Send + Default,
     {
         snapshot_storages
@@ -3941,6 +3941,7 @@ impl AccountsDB {
                         LoadedAccount::Stored(stored_account),
                         storage.append_vec_id(),
                         &mut retval,
+                        storage.slot(),
                     )
                 });
                 retval
@@ -3963,7 +3964,7 @@ impl AccountsDB {
         type ShardType = dashmap::lock::RwLock<
             std::collections::HashMap<
                 solana_sdk::pubkey::Pubkey,
-                dashmap::SharedValue<(u64, solana_sdk::hash::Hash, u64, u64)>,
+                dashmap::SharedValue<CalculateHashIntermediate>,
             >,
         >;
         let shards: &[ShardType] = account_maps.shards();
@@ -3976,7 +3977,7 @@ impl AccountsDB {
                     .iter()
                     .filter_map(|inp| {
                         let (pubkey, sv) = inp;
-                        let (_, hash, lamports, raw_lamports) = sv.get();
+                        let (_version, hash, lamports, raw_lamports, _slot) = sv.get();
                         if *raw_lamports != 0 {
                             Some((*pubkey, *hash, *lamports))
                         } else {
@@ -4080,7 +4081,8 @@ impl AccountsDB {
             storage,
             |loaded_account: LoadedAccount,
              _store_id: AppendVecId,
-             _accum: &mut Vec<(Pubkey, CalculateHashIntermediate)>| {
+             _accum: &mut Vec<(Pubkey, CalculateHashIntermediate)>,
+             slot: Slot| {
                 let public_key = loaded_account.pubkey();
                 let version = loaded_account.write_version();
                 let raw_lamports = loaded_account.lamports();
@@ -4097,10 +4099,14 @@ impl AccountsDB {
                     *loaded_account.loaded_hash(),
                     balance,
                     raw_lamports,
+                    slot,
                 );
                 match map.entry(*key) {
                     Occupied(mut dest_item) => {
-                        if dest_item.get_mut().version() <= source_item.version() {
+                        let contents = dest_item.get();
+                        if contents.4 < slot
+                            || (contents.4 == slot && contents.version() <= source_item.version())
+                        {
                             // replace the item
                             dest_item.insert(source_item);
                         }
