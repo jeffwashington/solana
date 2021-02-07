4acf2ee74bcbb78418b3441b9b4d5b37edbeceed
Jeff Washington (jwash) <wash678@gmail.com>
2021-02-06 23:09:12 -0600

    enable better

--- runtime/src/accounts_db.rs
+++ runtime/src/accounts_db.rs
@@ -3411,7 +3411,7 @@ impl AccountsDB {
         let total_hashes = hashes.len();
 
         let target = fanout * fanout * fanout;
-        let triple = false;//total_hashes >= target;
+        let triple = total_hashes >= target;
         let fanout_use = if triple {target} else {fanout};
         // we need div_ceil here
         let mut chunks = total_hashes / fanout_use;
