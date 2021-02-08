88db07ffaceed2d8daab18368b87b7ad2bdb9493
Jeff Washington (jwash) <wash678@gmail.com>
2021-02-08 15:45:28 -0600

    1/1k

--- runtime/src/accounts_db.rs
+++ runtime/src/accounts_db.rs
@@ -4183,7 +4183,7 @@ impl AccountsDB {
         let mut last3 = Pubkey::default();
         sorted_data_by_pubkey.iter().for_each(|v| {
             v.iter().for_each(|v| { 
-                if sum % 100 == 0 {
+                if sum % 1000 == 0 {
                     let now = v;
                     //let a1 = now.pubkey;//.as_ref();
                     //let a2 = last;//.as_ref();
