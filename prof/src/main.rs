8a498ff5504c4ec1d4b076adb5d9ab184378cde9
Jeff Washington (jwash) <wash678@gmail.com>
2021-02-09 09:31:15 -0600

    simpler

--- prof/src/main.rs
+++ prof/src/main.rs
@@ -12,19 +12,14 @@ loop {
 
     let sample = vec![0u8; size * 15_000_000];
     let now = Instant::now();
-    let mut sum = 0;
     let mut sum2 = 0;
     let factor = 1;
-    (0..sample.len()/(size * factor)).into_iter().for_each(|i|{
-        for j in 0..factor {
-            if sample[i*size*factor+j*size] != 1 {
-                sum2 += 1;
-            }
-
+    for i in 0..sample.len() {
+        if sample[i] != 1 {
+            sum2 += 1;
         }
     }
-    );
     let then = Instant::now();
     let dur = then - now;
-    println!("sum2: {}, time: {:?}, l: {}, factor: {}, size: {}", sum, dur, sum2, factor, size);
+    println!("sum2: {}, time: {:?}, factor: {}, size: {}", sum2, dur, factor, size);
 }}
