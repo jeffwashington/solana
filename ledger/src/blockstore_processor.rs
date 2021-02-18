df74d3a4d66e60bd08483327f941c30821fd6efe
Jeff Washington (jwash) <wash678@gmail.com>
2021-02-18 13:49:43 -0600

    no thread pool

--- ledger/src/blockstore_processor.rs
+++ ledger/src/blockstore_processor.rs
@@ -209,14 +209,12 @@ fn execute_batches(
     timej.stop(); timings.just_load += timej.as_us();
 
     let (results, new_timings): (Vec<Result<()>>, Vec<ExecuteTimings>) =
-    /*
-        PAR_THREAD_POOL.with(|thread_pool| {
+/*        PAR_THREAD_POOL.with(|thread_pool| {
             thread_pool.borrow().install(|| {*/
                 batches
-                    .into_iter()
-                    //.map_with(transaction_status_sender, |sender, batch| {
-                    .map(|batch| { let sender = &transaction_status_sender;
-                            let mut timings = ExecuteTimings::default();
+                    .into_par_iter()
+                    .map_with(transaction_status_sender, |sender, batch| {
+                        let mut timings = ExecuteTimings::default();
                         let result = execute_batch(
                             batch,
                             bank,
