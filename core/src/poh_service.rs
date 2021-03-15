21748862be933cd739b59b2850ae77b258daef1b
Jeff Washington (jwash) <wash678@gmail.com>
2021-03-15 18:34:58 -0500

    reset times

--- core/src/poh_service.rs
+++ core/src/poh_service.rs
@@ -232,7 +232,9 @@ impl PohService {
                     num_hashes = 0;
                     total_tick_time_ns = 0;
                     total_lock_time_ns = 0;
-                    total_hash_time_ns = 0;
+                    total_lock_time_record_ns = 0;
+                    total_lock_time_ns = 0;
+                    total_lock_time_poh_ns = 0;
                     last_metric = Instant::now();
                 }
                 if poh_exit.load(Ordering::Relaxed) {
