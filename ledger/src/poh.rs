902af520b17862223c379c62cbb7ca5f5ce3101a
Jeff Washington (jwash) <wash678@gmail.com>
2021-03-25 11:27:38 -0500

    metrics

--- ledger/src/poh.rs
+++ ledger/src/poh.rs
@@ -58,6 +58,12 @@ impl Poh {
         let elapsed_ns = (now - tick_start_time).as_nanos();
         let buffer_ns = 10; // report 10ns less
         let target_elapsed_ns = (target_ns_per_tick * num_hashes / hashes_per_tick - buffer_ns) as u128;
+        info!("delay_ns_to_let_wallclock_catchup, hashes: {}, hashes_per_tick: {}, elapsed_ns: {}, target_ns_per_tick: {}",
+            num_hashes,
+            hashes_per_tick,
+            elapsed_ns,
+            target_ns_per_tick,
+    );
         if elapsed_ns >= target_elapsed_ns {
             0
         } else {
