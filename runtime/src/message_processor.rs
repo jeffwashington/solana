53099f726b3e51ad06047655cbb767badc3527a8
Jeff Washington (jwash) <wash678@gmail.com>
2021-03-30 12:25:40 -0500

    a

--- runtime/src/message_processor.rs
+++ runtime/src/message_processor.rs
@@ -255,7 +255,7 @@ impl PreAccount {
                             ct += 1;
                         }
                     }
-                    error!("copy because different: ct: {}, diff byte: {}, key: {:?}", ct, diff, pre.key);
+                    error!("copy because different: ct: {}, diff byte: {}, key: {:?}", ct, diff, self.key);
                 }
                 pre.set_data_from_slice(&account.data());
             }
