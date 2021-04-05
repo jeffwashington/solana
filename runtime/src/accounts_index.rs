24745da3bf4eac30d9a1d0f88c6e34e73c562772
Jeff Washington (jwash) <wash678@gmail.com>
2021-04-04 23:49:09 -0500

    test cleanup

--- runtime/src/accounts_index.rs
+++ runtime/src/accounts_index.rs
@@ -1363,9 +1363,8 @@ pub mod tests {
             if slot % dead == 0 {
                 continue;
             }
-            let s2 = slot.clone();
-            hash.insert(s2);
-            bitfield.insert(s2);
+            hash.insert(slot);
+            bitfield.insert(slot);
         }
 
         let max = slot + 1;
@@ -1417,9 +1416,8 @@ pub mod tests {
             if slot % dead == 0 {
                 continue;
             }
-            let s2 = slot.clone();
-            hash.insert(s2);
-            bitfield.insert(s2);
+            hash.insert(slot);
+            bitfield.insert(slot);
         }
 
         let max = slot + 1;
