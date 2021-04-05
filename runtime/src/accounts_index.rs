aea2a4f30c6ad00dab8da301ddcbfbb5c21aac2e
Jeff Washington (jwash) <wash678@gmail.com>
2021-04-04 23:04:59 -0500

    us

--- runtime/src/accounts_index.rs
+++ runtime/src/accounts_index.rs
@@ -1449,7 +1449,7 @@ pub mod tests {
             "{}, {}, {}",
             time.as_ms(),
             time2.as_ms(),
-            time.as_ms() / time2.as_ms()
+            time.as_us() / time2.as_us()
         );
         assert_eq!(count, count2);
     }
