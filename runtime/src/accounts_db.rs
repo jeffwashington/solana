b6a9cfbde3b4487b2ebf5cb1b2a5d26030b7b76c
Jeff Washington (jwash) <wash678@gmail.com>
2021-04-22 10:22:18 -0500

    a

--- runtime/src/accounts_db.rs
+++ runtime/src/accounts_db.rs
@@ -1358,6 +1358,10 @@ impl AccountsDb {
         let mut reclaims = Vec::new();
         let mut dead_keys = Vec::new();
 
+        let pk1 = Pubkey::from_str("7jEfU57R2sV2B1DddKdsqZsdHaHm3B15REb4abvP6Me2").unwrap();
+        let pk2 = Pubkey::from_str("C57GmZLsPviiHZqWjYHo9is8QnMNq1Fc7SkYvLxLts24").unwrap();
+        let pk3 = Pubkey::from_str("3XA7qhMGS3UgbtyKSVo4rHuAm5yMmic3zKqb616QJDmz").unwrap();
+        let pk4 = Pubkey::from_str("9iDXA8wAvN3u4BhRoP1yL3n2PE8KxcFoNVbz1Xd9k7xw").unwrap();
         for (pubkey, slots_set) in pubkey_to_slot_set {
             let is_empty = self.accounts_index.purge_exact(
                 &pubkey,
@@ -1365,6 +1369,11 @@ impl AccountsDb {
                 &mut reclaims,
                 &self.account_indexes,
             );
+
+            if pubkey == &pk1 || pubkey == &pk2 || pubkey == &pk3 || pubkey == &pk4 {
+                error!("jwash:purge_keys_exact, {}, slots: {:?}, is_empty", pubkey, slots_set, is_empty);
+            }
+    
             if is_empty {
                 dead_keys.push(pubkey);
             }
