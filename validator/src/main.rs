cbd2ac62d4818604de949e822eb4d08dcb16f26f
Jeff Washington (jwash) <wash678@gmail.com>
2021-05-10 13:49:09 -0500

    a

--- validator/src/main.rs
+++ validator/src/main.rs
@@ -2527,13 +2527,19 @@ fn process_account_indexes(matches: &ArgMatches) -> AccountSecondaryIndexes {
     let account_indexes_include_keys: HashSet<Pubkey> = matches
         .values_of("account_index_include_key")
         .unwrap_or_default()
-        .map(|value| Pubkey::from_str(value).expect("invalid pubkey"))
+        .map(|value| {
+            error!("pubkey: {}", value);
+            Pubkey::from_str(value).expect("invalid pubkey")
+        })
         .collect();
 
     let account_indexes_exclude_keys: HashSet<Pubkey> = matches
         .values_of("account_index_exclude_key")
         .unwrap_or_default()
-        .map(|value| {error!("pubkey: {}", value); Pubkey::from_str(value).expect("invalid pubkey")})
+        .map(|value| {
+            error!("pubkey: {}", value);
+            Pubkey::from_str(value).expect("invalid pubkey")
+        })
         .collect();
 
     let exclude_keys = !account_indexes_exclude_keys.is_empty();
