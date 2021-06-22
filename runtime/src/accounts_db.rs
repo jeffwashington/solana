a55e6367de00d1298d46ff36131e1e5cbe3c82e1
Jeff Washington (jwash) <wash678@gmail.com>
2021-06-22 16:07:48 -0500

    min/max, 16

--- runtime/src/accounts_db.rs
+++ runtime/src/accounts_db.rs
@@ -6036,14 +6036,30 @@ impl AccountsDb {
             insertion_time_us: insertion_time.load(Ordering::Relaxed),
         };
         timings.report();
-        error!("total items in index: {}", self
+        error!("total items in index: {}, {}, {}", self
         .accounts_index
         .account_maps
         .read()
         .unwrap()
         .iter()
         .map(|i| i.len())
-        .sum::<usize>());
+        .sum::<usize>(),
+        self
+        .accounts_index
+        .account_maps
+        .read()
+        .unwrap()
+        .iter()
+        .map(|i| i.len())
+        .min().unwrap(),
+        self
+        .accounts_index
+        .account_maps
+        .read()
+        .unwrap()
+        .iter()
+        .map(|i| i.len())
+        .max() .unwrap()   );
         panic!("tha's enough");
 
         // Need to add these last, otherwise older updates will be cleaned
