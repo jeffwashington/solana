bb3c408d37e6a58e82d4ced605b62ade0eb4d04a
Jeff Washington (jwash) <wash678@gmail.com>
2021-06-25 15:29:32 -0500

    1m

--- runtime/src/seekable_buffering_reader.rs
+++ runtime/src/seekable_buffering_reader.rs
@@ -87,7 +87,7 @@ impl SeekableBufferingReader {
     }
     fn read_entire_file_in_bg<T: 'static + Read + std::marker::Send>(&self, mut reader: T) {
         let mut time = Measure::start("");
-        const CHUNK_SIZE: usize = 10_000_000;
+        const CHUNK_SIZE: usize = 1_000_000;
         let (_lock, cvar) = &self.instance.new_data_signal;
         let mut notify = 0;
         let mut read = 0;
