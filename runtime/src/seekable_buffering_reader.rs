5d7b7a376cf95d28068330cfb2689898909d6691
Jeff Washington (jwash) <wash678@gmail.com>
2021-06-29 13:06:14 -0500

    tighter common read

--- runtime/src/seekable_buffering_reader.rs
+++ runtime/src/seekable_buffering_reader.rs
@@ -478,9 +478,32 @@ impl Read for SeekableBufferingReader {
     fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
         let request_len = buf.len();
         let now = std::time::Instant::now();
-
         let mut remaining_request = request_len;
         let mut offset_in_dest = 0;
+        match &self.current_data {
+            Some(source) => {
+                let source = &*source;
+                let full_len = source.len();
+                let remaining_len = full_len - self.next_index_within_last_buffer;
+                let bytes_to_transfer = std::cmp::min(remaining_len, request_len);
+
+                // copy what we can
+                let mut m = Measure::start("");
+                buf[offset_in_dest..(offset_in_dest + bytes_to_transfer)].copy_from_slice(
+                    &source[self.next_index_within_last_buffer
+                        ..(self.next_index_within_last_buffer + bytes_to_transfer)],
+                );
+                m.stop();
+                self.copy_data += m.as_us();
+                self.next_index_within_last_buffer += bytes_to_transfer;
+                offset_in_dest += bytes_to_transfer;
+                remaining_request -= bytes_to_transfer;
+            }
+            None => {
+                // fall through and get us a local buffer
+            }
+        }
+
         let mut eof_seen = false;
         while remaining_request > 0 {
             match &self.current_data {
