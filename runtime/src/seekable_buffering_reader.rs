0482b3402b1a0ccd09b77788a54aa263b4f468f1
Jeff Washington (jwash) <wash678@gmail.com>
2021-06-29 10:49:51 -0500

    100m buffer

--- runtime/src/seekable_buffering_reader.rs
+++ runtime/src/seekable_buffering_reader.rs
@@ -72,9 +72,9 @@ impl Drop for SeekableBufferingReaderInner {
     }
 }
 
-const TOTAL_BUFFER_BUDGET: usize = 200_000_000;
-const CHUNK_SIZE: usize = 10_000_000;
-const MAX_READ_SIZE: usize = 10_000_000; //65536*2;
+const TOTAL_BUFFER_BUDGET: usize = 2_000_000_000;
+const CHUNK_SIZE: usize = 100_000_000;
+const MAX_READ_SIZE: usize = 100_000_000; //65536*2;
 
 impl SeekableBufferingReader {
     fn new_with_instance(instance: &Arc<SeekableBufferingReaderInner>) -> Self {
