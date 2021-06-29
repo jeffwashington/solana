f9c705cd554eaa251d86f1b6a4a26761cadcf16d
Jeff Washington (jwash) <wash678@gmail.com>
2021-06-29 13:30:11 -0500

    min

--- runtime/src/seekable_buffering_reader.rs
+++ runtime/src/seekable_buffering_reader.rs
@@ -439,11 +439,8 @@ impl SeekableBufferingReader {
         drop(indices);
         let indices = self.instance.clients.read().unwrap();
         let mut new_min = *indices.iter().min().unwrap();
-        if new_min == usize::MAX {
-            new_min = self.instance.data.read().unwrap().len(); // we are done, we can drop all the rest
-                                                                //error!("moved: {}, new min is: {}", self.my_client_index, new_min);
-        }
-        error!("update_client_index: {}, {}, new min is: {}, sizes: {:?}", self.my_client_index, last_buffer_index, new_min, *self.instance.clients.read().unwrap());
+        new_min = std::cmp::min(new_min, self.instance.data.read().unwrap().len());
+        //error!("update_client_index: {}, {}, new min is: {}, sizes: {:?}", self.my_client_index, last_buffer_index, new_min, *self.instance.clients.read().unwrap());
         drop(indices);
         if new_min > previous_last_buffer_index {
             let eof = self.reached_eof();
