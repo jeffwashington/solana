9b25a502eaaf858e81fb54fd67e3bf0eeb8ef70b
Jeff Washington (jwash) <wash678@gmail.com>
2021-06-29 13:55:28 -0500

    a

--- runtime/src/seekable_buffering_reader.rs
+++ runtime/src/seekable_buffering_reader.rs
@@ -254,7 +254,7 @@ impl SeekableBufferingReader {
                     drop(buffers);
                     match buffer {
                         Some(buffer) => {
-                            error!("got buffer: idx: {}, remainng: {}", division_index, remaining);
+                            //error!("got buffer: idx: {}, remainng: {}", division_index, remaining);
                             dest_data = buffer;
                             read_this_much = dest_data.len();
                             assert_eq!(Arc::strong_count(&dest_data), 1);
