2409b32421efed1a10a5ac541a1767aeeb693f00
Jeff Washington (jwash) <wash678@gmail.com>
2021-06-29 14:20:13 -0500

    8B

--- runtime/src/seekable_buffering_reader.rs
+++ runtime/src/seekable_buffering_reader.rs
@@ -80,7 +80,7 @@ impl Drop for SeekableBufferingReaderInner {
     }
 }
 
-const TOTAL_BUFFER_BUDGET: usize = 4_000_000_000;
+const TOTAL_BUFFER_BUDGET: usize = 8_000_000_000;
 const CHUNK_SIZE: usize = 100_000_000;
 const MAX_READ_SIZE: usize = 100_000_000; //65536*2;
 
