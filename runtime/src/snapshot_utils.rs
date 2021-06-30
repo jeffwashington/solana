9c33c7565b5844987dab4272bda388a8a9cf75cf
Jeff Washington (jwash) <wash678@gmail.com>
2021-06-30 11:06:40 -0500

    eliminate startup race condition

--- runtime/src/snapshot_utils.rs
+++ runtime/src/snapshot_utils.rs
@@ -796,15 +796,19 @@ fn unpack_snapshot_local<T: 'static + Read + std::marker::Send, F: Fn() -> T>(
     // a shared 'reader' that reads the decompressed stream once, keeps some history, and acts as a reader for multiple parallel archive readers
     let shared_buffer = SharedBuffer::new(reader());
 
+    // allocate all readers before any readers start reading
+    let readers = (0..parallel_archivers).into_iter().map(|_| SharedBufferReader::new(&shared_buffer)).collect::<Vec<_>>();
+
     // create 'divisions' # of parallel workers, each responsible for 1/divisions of all the files to extract.
-    let all_unpacked_append_vec_map = (0..parallel_archivers)
+    let all_unpacked_append_vec_map = readers
         .into_par_iter()
-        .map(|index| {
+        .enumerate()
+        .map(|(index, reader)| {
             let parallel_selector = Some(ParallelSelector {
                 index,
                 divisions: parallel_archivers,
             });
-            let mut archive = Archive::new(SharedBufferReader::new(&shared_buffer));
+            let mut archive = Archive::new(reader);
             unpack_snapshot(&mut archive, ledger_dir, account_paths, parallel_selector)
         })
         .collect::<Vec<_>>();
