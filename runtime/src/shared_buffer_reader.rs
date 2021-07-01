4fe728892c4cecc1b736f7ba750e2701958302df
Jeff Washington (jwash) <wash678@gmail.com>
2021-07-01 16:12:29 -0500

    a

--- runtime/src/shared_buffer_reader.rs
+++ runtime/src/shared_buffer_reader.rs
@@ -378,10 +378,10 @@ impl Read for SharedBufferReader {
                     [self.index_in_current_data..(self.index_in_current_data + bytes_to_transfer)],
             );
             if offset_in_dest > 0 && bytes_to_transfer > 0  {
-                assert!(buf[offset_in_dest -1] + 1 == buf[offset_in_dest]);
+                assert!((buf[offset_in_dest -1] as usize + 1) % 256 == buf[offset_in_dest]);
             }
             for i in 1..offset_in_dest {
-                assert_eq!(buf[i], buf[i-1] + 1);
+                assert_eq!(buf[i] as usize, (buf[i-1] as usize + 1) % 256);
             }
             self.index_in_current_data += bytes_to_transfer;
             offset_in_dest += bytes_to_transfer;
