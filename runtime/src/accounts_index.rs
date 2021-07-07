86b5a558bee9ae2354048a85aaaf50ba1ade2d07
Jeff Washington (jwash) <wash678@gmail.com>
2021-07-06 23:22:04 -0500

    16 bins

--- runtime/src/accounts_index.rs
+++ runtime/src/accounts_index.rs
@@ -31,7 +31,7 @@ use std::{
 use thiserror::Error;
 
 pub const ITER_BATCH_SIZE: usize = 1000;
-const BINS: usize = 64;
+const BINS: usize = 16;
 pub type ScanResult<T> = Result<T, ScanError>;
 pub type SlotList<T> = Vec<(Slot, T)>;
 pub type SlotSlice<'s, T> = &'s [(Slot, T)];
