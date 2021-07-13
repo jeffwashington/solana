3346c37b0ca178656bcc65fdb0bd739f981e8cc8
Jeff Washington (jwash) <wash678@gmail.com>
2021-07-13 14:17:22 -0500

    log

--- runtime/src/accounts_index.rs
+++ runtime/src/accounts_index.rs
@@ -776,7 +776,9 @@ impl<T: 'static + Clone + IsCached + ZeroLamport + std::marker::Sync + std::mark
         self.account_maps.par_iter().for_each(|m| {
             m.read().unwrap().flush();
         });
+        error!("flush done");
         error!("distribution: {:?}", self.account_maps.first().unwrap().read().unwrap().distribution());
+        error!("distribution done");
     }
     fn iter<R>(&self, range: Option<R>) -> AccountsIndexIterator<T>
     where
