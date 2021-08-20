3afcdbf856542b3a1593e05a11609ab563b2169a
Jeff Washington (jwash) <wash678@gmail.com>
2021-08-20 16:58:10 -0500

    74394692

--- runtime/src/accounts_db.rs
+++ runtime/src/accounts_db.rs
@@ -1867,7 +1867,7 @@ impl AccountsDb {
                                 AccountIndexGetResult::Found(locked_entry, index) => {
                                     let slot_list = locked_entry.slot_list();
                                     let (slot, account_info) = &slot_list[index];
-                                    if pubkey == &pk1 || slot == &73365341 {
+                                    if pubkey == &pk1 || slot == &74394692 {
                                         error!("{} {} clean_accounts {}, slot list: {:?}, info: {:?}, roots and refcounts: {:?}", file!(), line!(), pubkey, slot_list, account_info,
                                         self.accounts_index
                                         .roots_and_ref_count(&locked_entry, max_clean_root)                                    );
@@ -2251,7 +2251,7 @@ impl AccountsDb {
     where
         I: Iterator<Item = &'a Arc<AccountStorageEntry>>,
     {
-        if slot == 73365341 {
+        if slot == 74394692 {
             error!("{} {} do_shrink_slot_stores {}", file!(), line!(), slot);
         }
         struct FoundStoredAccount<'a> {
@@ -2303,7 +2303,7 @@ impl AccountsDb {
                     i.store_id == stored_account.store_id
                         && i.offset == stored_account.account.offset
                 });
-                if pubkey == &pk1 || slot == 73365341 {
+                if pubkey == &pk1 || slot == 74394692 {
                     error!("{} {} do_shrink_slot_stores {}, alive: {}, slot: {}", file!(), line!(), pubkey,is_alive, slot);
                 }
                 if !is_alive {
@@ -2500,7 +2500,7 @@ impl AccountsDb {
     // then create a minimum AppendVec filled with the alive.
     fn shrink_slot_forced(&self, slot: Slot, is_startup: bool) -> usize {
         debug!("shrink_slot_forced: slot: {}", slot);
-        if slot == 73365341 {
+        if slot == 74394692 {
             error!("{} {} shrink_slot_forced {}", file!(), line!(), slot);
         }
 
@@ -2508,14 +2508,14 @@ impl AccountsDb {
             let stores: Vec<Arc<AccountStorageEntry>> =
                 stores_lock.read().unwrap().values().cloned().collect();
             if !Self::is_shrinking_productive(slot, &stores) {
-                if slot == 73365341 {
+                if slot == 74394692 {
                     error!("{} {} shrink_slot_forced {}, !productive", file!(), line!(), slot);
                 }
                         return 0;
             }
             self.do_shrink_slot_stores(slot, stores.iter(), is_startup)
         } else {
-            if slot == 73365341 {
+            if slot == 74394692 {
                 error!("{} {} shrink_slot_forced not found {}", file!(), line!(), slot);
             }
                 0
@@ -4792,7 +4792,7 @@ impl AccountsDb {
             let mut account = (0, None);
             std::mem::swap(&mut account, &mut current[min_index]);
             ct += 1;
-            if slot == 73365341 {
+            if slot == 74394692 {
                 error!("{} {}", slot, account.1.as_ref().unwrap().meta.pubkey);
             }
             scan_func(
@@ -4815,7 +4815,7 @@ impl AccountsDb {
                 }
             }
         }
-        if slot == 73365341 { //old {
+        if slot == 74394692 { //old {
             error!("slot: {}, count: {}", slot, ct);
         }
     }
@@ -5599,7 +5599,7 @@ impl AccountsDb {
 
         let aligned_bytes = Self::page_align(alive_bytes as u64);
         if Self::should_not_shrink(aligned_bytes, total_bytes, stores.len()) {
-            if slot == 73365341 {
+            if slot == 74394692 {
                 error!("{} {} shrink_slot_forced ({}, {}): not able to shrink at all: alive/stored: ({} / {}) ({}b / {}b) save: {}",
                 file!(), line!(),                slot,
                 stores.len(),
@@ -5769,7 +5769,7 @@ impl AccountsDb {
                     .entry(pubkey)
                     .or_default()
                     .insert(slot);
-                if pubkey == pk1 || slot == 73365341 || slot == 73365341 {
+                if pubkey == pk1 || slot == 74394692 || slot == 74394692 {
                     error!("{} {} clean_dead_slots_from_accounts_index {} unref_from_storage", file!(), line!(), pubkey,);
                 }
 
@@ -5782,7 +5782,7 @@ impl AccountsDb {
         let mut unrooted_cleaned_count = 0;
         let _dead_slots: Vec<_> = dead_slots_iter
             .map(|slot| {
-                if slot == &73365341 || slot == &73365341 {
+                if slot == &74394692 || slot == &74394692 {
                     error!("{} {} clean_dead_slots_from_accounts_index not specific to this pk: {}, slot: {}", file!(), line!(), pk1,slot,);
                 }
                 if let Some(latest) = self.accounts_index.clean_dead_slot(*slot) {
@@ -5818,11 +5818,11 @@ impl AccountsDb {
                 }
             }
         }
-        if dead_slots.contains(&73365341) {
-            error!("{} {} clean_stored_dead_slots {}", file!(), line!(), 73365341);
+        if dead_slots.contains(&74394692) {
+            error!("{} {} clean_stored_dead_slots {}", file!(), line!(), 74394692);
         }
-        if dead_slots.contains(&73365341) {
-            error!("{} {} clean_stored_dead_slots {}", file!(), line!(), 73365341);
+        if dead_slots.contains(&74394692) {
+            error!("{} {} clean_stored_dead_slots {}", file!(), line!(), 74394692);
         }
         error!("{} {} clean_stored_dead_slots {}", file!(), line!(), dead_slots.len());
 
@@ -6358,7 +6358,7 @@ impl AccountsDb {
                     );
                 }
 
-                if pubkey == pk1 || pubkey == pk2 || slot == &73365341 || slot == &73365341 {
+                if pubkey == pk1 || pubkey == pk2 || slot == &74394692 || slot == &74394692 {
                     error!("loading: {} {} {:?}, items in slot: {}", slot, pubkey, stored_account.account_meta.lamports, len);
                 }
 
