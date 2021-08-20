f5082f8fa0ee7be7f2d8648998df02b411563173
Jeff Washington (jwash) <wash678@gmail.com>
2021-08-20 15:43:36 -0500

    ce

--- runtime/src/accounts_db.rs
+++ runtime/src/accounts_db.rs
@@ -2221,8 +2221,8 @@ impl AccountsDb {
             return;
         }
         let mut clean_dead_slots = Measure::start("reclaims::clean_dead_slots");
+        error!("{} {} process_dead_slots {}", file!(), line!(), purged_account_slots.as_ref().map(|x| x.len()).unwrap_or_default());
         self.clean_stored_dead_slots(dead_slots, purged_account_slots);
-        error!("{} {} process_dead_slots {}", file!(), line!(), purged_account_slots.map(|x| x.len()).as_ref().unwrap_or_default());
         clean_dead_slots.stop();
 
         let mut purge_removed_slots = Measure::start("reclaims::purge_removed_slots");
