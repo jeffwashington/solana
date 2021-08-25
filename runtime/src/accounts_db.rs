d25cce2b04181dd8f97d0be5a8303ada7eac8ff7
Jeff Washington (jwash) <wash678@gmail.com>
2021-08-25 14:23:08 -0500

    log old slot contents

--- runtime/src/accounts_db.rs
+++ runtime/src/accounts_db.rs
@@ -4703,6 +4703,61 @@ impl AccountsDb {
         check_hash: bool,
         can_cached_slot_be_unflushed: bool,
     ) -> Result<(Hash, u64), BankHashVerificationError> {
+        {
+            let combined_maps = self.get_snapshot_storages(slot, None).0;
+
+            let mut slots = vec![];
+            let mut max = 0;
+            for s in combined_maps.iter() {
+                let slot = s.first().unwrap().slot();
+                max = std::cmp::max(max, slot);
+                slots.push((slot, s.first().unwrap().clone()));
+            }
+            slots.sort_unstable_by(|a, b| a.0.cmp(&b.0));
+            let first_expected_slot = max - 432_000 - 100;
+            error!(
+                "range before scan: {}",
+                max - slots.first().map(|slot| slot.0).unwrap_or_default()
+            );
+            let mut count_old = 0;
+            for (slot, store) in slots {
+                if slot >= first_expected_slot {
+                    break;
+                }
+                count_old += 1;
+
+
+                error!(
+                    "unexpected old root: {}, count_and_status: {:?}, alive_bytes: {}",
+                    slot,
+                    store.count_and_status.read().unwrap(),
+                    store.alive_bytes.load(Ordering::Relaxed)
+                );
+                let accounts = store.accounts.accounts(0);
+                accounts.into_iter().for_each(|stored_account| {
+                    let la = LoadedAccount::Stored(stored_account);
+                    let key = la.pubkey();
+                    let read = self.accounts_index.account_maps
+                        [crate::accounts_index::get_bin_pubkey(la.pubkey())]
+                    .read()
+                    .unwrap();
+                    let v = read.get(key);
+                    error!(
+                        "accounts_in_store:{},{}, ref_count_from_storage: {}, index rc: {}",
+                        la.pubkey(),
+                        la.lamports(),
+                        self.accounts_index.ref_count_from_storage(la.pubkey()),
+                        v.map(|x| x.ref_count()).unwrap_or_default()
+                    );
+                    if let Some(v) = v {
+                        for i in v.slot_list.read().unwrap().iter() {
+                            error!("accounts_index: {:?}", i);
+                        }
+                    }
+                });
+            }
+            error!("old stores added to dirty_stores: {}", count_old);
+        }
         if !use_index {
             let accounts_cache_and_ancestors = if can_cached_slot_be_unflushed {
                 Some((&self.accounts_cache, ancestors, &self.accounts_index))
