678adc7a132984c0c91131879266ce23db742fc0
Jeff Washington (jwash) <wash678@gmail.com>
2021-09-07 02:16:34 -0500

    worse throughput

--- runtime/src/bucket_map_holder.rs
+++ runtime/src/bucket_map_holder.rs
@@ -25,7 +25,7 @@ use std::time::Duration;
 
 // how many times should we be able to iterate the entire cache and complete a flush in 1000s
 // 1000 = 1 iteration/second
-const FULL_FLUSHES_PER_1000_S: usize = 1000;
+const FULL_FLUSHES_PER_1000_S: usize = 100;
 const MAX_THREADS: usize = 8;
 const THROUGHPUT_POLL_MS: u64 = 200;
 
