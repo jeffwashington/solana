3086c2b24be6d86e5abe810d4a1dc42b1b58b95a
Jeff Washington (jwash) <wash678@gmail.com>
2021-09-22 13:48:21 -0500

    advance age when startup ends

--- runtime/src/bucket_map_holder.rs
+++ runtime/src/bucket_map_holder.rs
@@ -79,7 +79,11 @@ impl<T: IndexValue> BucketMapHolder<T> {
         if !value {
             self.wait_for_idle();
         }
-        self.startup.store(value, Ordering::Relaxed)
+        self.startup.store(value, Ordering::Release);
+        // now that we are not startup, get the system moving normally
+        self.maybe_advance_age();
+        self.wait_dirty_or_aged
+            .wait_timeout(Duration::from_millis(AGE_MS));
     }
 
     pub(crate) fn wait_for_idle(&self) {
@@ -160,15 +164,19 @@ impl<T: IndexValue> BucketMapHolder<T> {
         let bins = in_mem.len();
         let flush = self.disk.is_some();
         loop {
-            if self.all_buckets_flushed_at_current_age() {
+            let timeout = if self.all_buckets_flushed_at_current_age() {
                 let mut m = Measure::start("wait");
-                self.wait_dirty_or_aged
+                let timeout = self
+                    .wait_dirty_or_aged
                     .wait_timeout(Duration::from_millis(AGE_MS));
                 m.stop();
                 self.stats
                     .bg_waiting_us
                     .fetch_add(m.as_us(), Ordering::Relaxed);
-            }
+                timeout
+            } else {
+                false
+            };
 
             if exit.load(Ordering::Relaxed) {
                 break;
