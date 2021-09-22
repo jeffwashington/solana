c871c8e09badfe0e95c6833e10d6c3ae0691e30e
Jeff Washington (jwash) <wash678@gmail.com>
2021-09-22 15:19:27 -0500

    ce

--- runtime/src/bucket_map_holder.rs
+++ runtime/src/bucket_map_holder.rs
@@ -88,12 +88,14 @@ impl<T: IndexValue> BucketMapHolder<T> {
         use log::*;
         error!("wait for idle starting. items in mem: {}", self.stats.count_in_mem.load(Ordering::Relaxed));
         assert!(self.get_startup());
-        if self.stats.count_in_mem.load(Ordering::Relaxed) == 0 {
-            // all in_mem buckets are empty, so we flushed correctly
-            error!("wait for idle quitting. items in mem: {}", self.stats.count_in_mem.load(Ordering::Relaxed));
-            break;
+        loop {
+            if self.stats.count_in_mem.load(Ordering::Relaxed) == 0 {
+                // all in_mem buckets are empty, so we flushed correctly
+                error!("wait for idle quitting. items in mem: {}", self.stats.count_in_mem.load(Ordering::Relaxed));
+                break;
+            }
+            std::thread::sleep(Duration::from_millis(100));
         }
-        std::thread::sleep(Duration::from_millis(100));
     }
 
     pub fn current_age(&self) -> Age {
