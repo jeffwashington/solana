f2fe3924374e14f9441aac21a3b53886f745a024
Jeff Washington (jwash) <wash678@gmail.com>
2021-11-15 15:46:20 -0600

    reorder

--- bucket_map/benches/bucket_map.rs
+++ bucket_map/benches/bucket_map.rs
@@ -36,8 +36,8 @@ use test::Bencher;
 type IndexValue = (usize, usize, usize, u64);
 
 DEFINE_NxM_BENCH!(dim_01x01, 1, 10, 32);
-DEFINE_NxM_BENCH!(dim_01x02a, 1, 10_000, 32);
-DEFINE_NxM_BENCH!(dim_01x02b, 1, 100_000, 32);
+DEFINE_NxM_BENCH!(dim_01x01a, 1, 10_000, 32);
+DEFINE_NxM_BENCH!(dim_01x02, 1, 100_000, 32);
 DEFINE_NxM_BENCH!(dim_01x04, 1, 1000_000, 32);
 DEFINE_NxM_BENCH!(dim_01x08, 1, 1000_000, 16);
 DEFINE_NxM_BENCH!(dim_01x16, 1, 1000_000, 64);
