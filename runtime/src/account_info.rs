32420605a53f6d3154467f5f486434216e871561
Jeff Washington (jwash) <wash678@gmail.com>
2021-12-13 12:37:18 -0600

    AcctIdx: move zero lamport out of accounts index

--- runtime/src/account_info.rs
+++ runtime/src/account_info.rs
@@ -1,3 +1,7 @@
+//! AccountInfo represents a reference to AccountSharedData in either an AppendVec or the write cache.
+//! AccountInfo is not persisted anywhere between program runs.
+//! AccountInfo is purely runtime state.
+//! Note that AccountInfo is saved to disk buckets during runtime, but disk buckets are recreated at startup.
 use crate::{accounts_db::AppendVecId, accounts_index::ZeroLamport};
 
 #[derive(Default, Debug, PartialEq, Clone, Copy)]
@@ -10,30 +14,39 @@ pub struct AccountInfo {
 
     /// needed to track shrink candidacy in bytes. Used to update the number
     /// of alive bytes in an AppendVec as newer slots purge outdated entries
+    /// Note that highest bit is used for ZERO_LAMPORT_BIT
     stored_size: usize,
-
-    /// lamports in the account used when squashing kept for optimization
-    /// purposes to remove accounts with zero balance.
-    lamports: u64,
 }
+
+/// presence of this bit in stored_size indicates this account info references an account with zero lamports
+const ZERO_LAMPORT_BIT: usize = 1 << (usize::BITS - 1);
 
 impl ZeroLamport for AccountInfo {
     fn is_zero_lamport(&self) -> bool {
-        self.lamports == 0
+        self.stored_size & ZERO_LAMPORT_BIT == ZERO_LAMPORT_BIT
     }
 }
 
 impl AccountInfo {
-    pub fn new(store_id: AppendVecId, offset: usize, stored_size: usize, lamports: u64) -> Self {
+    pub fn new(
+        store_id: AppendVecId,
+        offset: usize,
+        mut stored_size: usize,
+        lamports: u64,
+    ) -> Self {
+        assert!(stored_size < ZERO_LAMPORT_BIT);
+        if lamports == 0 {
+            stored_size |= ZERO_LAMPORT_BIT;
+        }
         Self {
             store_id,
             offset,
             stored_size,
-            lamports,
         }
     }
 
     pub fn stored_size(&self) -> usize {
-        self.stored_size
+        // elminate the special bit that indicates the info references an account with zero lamports
+        self.stored_size & !ZERO_LAMPORT_BIT
     }
 }
