ef164f2971848d5846f810a611f9337185d881fb
Jeff Washington (jwash) <wash678@gmail.com>
2022-01-31 13:15:54 -0600

    add hash after verify

--- ledger/src/blockstore_processor.rs
+++ ledger/src/blockstore_processor.rs
@@ -1305,6 +1305,7 @@ fn load_frozen_forks(
             )?;
 
             if slot >= dev_halt_at_slot {
+                bank.verify_bank_hash(true);
                 break;
             }
         }
