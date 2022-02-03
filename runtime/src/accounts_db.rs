f51becb193eb4a5ee31f4f7fee4a8404cdce58fa
Jeff Washington (jwash) <wash678@gmail.com>
2022-02-03 09:25:14 -0600

    ce

--- runtime/src/accounts_db.rs
+++ runtime/src/accounts_db.rs
@@ -5795,8 +5795,7 @@ impl AccountsDb {
                                             let find_next_slot = |slot: Slot| {
                                                 let roots = self.accounts_index.get_next_original_root(slot);
                                                 if roots.is_none() {
-                                                    error!("not next slot2: {}, max: {}", slot, maybe_db
-                                                    .unwrap().accounts_index.max_root());
+                                                    error!("not next slot2: {}, max: {}", slot, self.accounts_index.max_root());
                                                 }
                                                 roots
                                                                             };
