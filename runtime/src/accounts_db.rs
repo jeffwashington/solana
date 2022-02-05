076f56c0bcefb711165acfb90bc1fec3b1bf2a30
Jeff Washington (jwash) <wash678@gmail.com>
2022-02-04 20:45:37 -0600

    interesting id

--- runtime/src/accounts_db.rs
+++ runtime/src/accounts_db.rs
@@ -5767,7 +5767,7 @@ impl AccountsDb {
                             if self.is_filler_account(pubkey) {
                                 return None;
                             }
-                            let mut interesting =         pubkey == &Pubkey::from_str("C9AiH5qDN2fkz2QG2SUxWcRk5fBZgvbD5KZ7s1nWF1oe").unwrap();
+                            let mut interesting =         pubkey == &Pubkey::from_str("51ziC7nFBiY6vbBg4LWf6NeFywfntSHnXFuJtZMBoT6x").unwrap();
                             let search = self.accounts_index.get(pubkey, Some(ancestors), Some(max_root));
                             if let AccountIndexGetResult::Found(lock, index) =
                                 search
@@ -6430,7 +6430,7 @@ impl AccountsDb {
         let interesting_slot = 116501869;
         let interesting_parittion = 100732;
         let interesting = true && (pubkey
-            == &Pubkey::from_str("C9AiH5qDN2fkz2QG2SUxWcRk5fBZgvbD5KZ7s1nWF1oe").unwrap()
+            == &Pubkey::from_str("51ziC7nFBiY6vbBg4LWf6NeFywfntSHnXFuJtZMBoT6x").unwrap()
             || ((storage_slot == interesting_slot || storage_slot == 115044876)
                 && partition_index_from_max_slot == partition_from_pubkey)) || partition_from_pubkey == interesting_parittion;
 
@@ -6549,7 +6549,7 @@ impl AccountsDb {
         }
 
         /*
-        if pubkey == &Pubkey::from_str("C9AiH5qDN2fkz2QG2SUxWcRk5fBZgvbD5KZ7s1nWF1oe").unwrap() {
+        if pubkey == &Pubkey::from_str("51ziC7nFBiY6vbBg4LWf6NeFywfntSHnXFuJtZMBoT6x").unwrap() {
         }
         */
         assert!(!force_rehash);
@@ -6730,7 +6730,7 @@ impl AccountsDb {
                 );
 
                 let interesting = true && pubkey
-                    == &Pubkey::from_str("C9AiH5qDN2fkz2QG2SUxWcRk5fBZgvbD5KZ7s1nWF1oe").unwrap();
+                    == &Pubkey::from_str("51ziC7nFBiY6vbBg4LWf6NeFywfntSHnXFuJtZMBoT6x").unwrap();
 
                 let source_item = CalculateHashIntermediate::new(hash, balance, *pubkey);
                 if interesting {
@@ -7090,7 +7090,7 @@ impl AccountsDb {
                     let old_slot = pubkey_account.2;
                     use std::str::FromStr;
                     let mut interesting = pubkey
-                        == &Pubkey::from_str("C9AiH5qDN2fkz2QG2SUxWcRk5fBZgvbD5KZ7s1nWF1oe")
+                        == &Pubkey::from_str("51ziC7nFBiY6vbBg4LWf6NeFywfntSHnXFuJtZMBoT6x")
                             .unwrap();
                     if interesting {
                         error!(
