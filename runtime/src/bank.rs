b06d432fc82030d977243f1210e86d76c3f8e8ab
Jeff Washington (jwash) <wash678@gmail.com>
2022-02-08 15:05:09 -0600

    80

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -4622,13 +4622,14 @@ impl Bank {
                     collected2.push((pubkey, account.rent_epoch(), account.lamports()));
                 }
             } else {
+                // 80 
                 // 160 is fail
                 // 240 fail
                 // 280 fail
                 // 300 fail
                 // 321
 
-                if self.rewrites.len() > 310 {
+                if self.rewrites.len() > 80 {
                     first = false;
                 }
                 //first = false;
