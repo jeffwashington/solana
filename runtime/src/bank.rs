4ce535344762da25b4713defca63da8e2ddfe84b
Jeff Washington (jwash) <wash678@gmail.com>
2022-02-10 12:49:35 -0600

    103

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -4635,11 +4635,12 @@ impl Bank {
             } else {
                 // 0 GzPr6qkupyuXnmHdV1AyQYHH1Qt1VH7NbNrj68C3VyHy good
                 // > 83  GzPr6qkupyuXnmHdV1AyQYHH1Qt1VH7NbNrj68C3VyHy
+                // > 124 EA6vgwyDbq5qYVaptRJybAh3MhZ1gbf2C2YNxFjoJFGo
                 // > 167 EA6vgwyDbq5qYVaptRJybAh3MhZ1gbf2C2YNxFjoJFGo  
                 // > 237  
                 // 334 EA6vgwyDbq5qYVaptRJybAh3MhZ1gbf2C2YNxFjoJFGo
                 // 1317 
-                if self.rewrites.len() > 124 {
+                if self.rewrites.len() > 103 {
                     first = false;
                 }
                 //first = false;
