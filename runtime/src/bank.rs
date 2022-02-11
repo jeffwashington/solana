99f27ed3c39eb34c54e3fe7d46307f4ee031af58
Jeff Washington (jwash) <wash678@gmail.com>
2022-02-10 21:15:49 -0600

    120253357

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -4593,8 +4593,8 @@ impl Bank {
         > 119675200 2hX
           119675231
         */
-        let slot_interesting_here = self.slot() == 119675231;// self.slot() == 119675183;
-        let mut first = true;//slot_interesting_here;//true || (self.slot() >= 115929262 && self.slot() <= 115929262); //115929302; //false;
+        let slot_interesting_here = self.slot() == 120253357;// self.slot() == 119675183;
+        let mut first = slot_interesting_here;//true || (self.slot() >= 115929262 && self.slot() <= 115929262); //115929302; //false;
                                                                                         // parallelize?
         let mut rent_debits = RentDebits::default();
         let mut skipped = vec![];
