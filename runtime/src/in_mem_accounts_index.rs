745249d047d4d295663c2fb18dea4fd65c18fd6f
Jeff Washington (jwash) <wash678@gmail.com>
2022-03-16 11:45:56 -0500

    AcctIdx: check for range holds outside lock

--- runtime/src/in_mem_accounts_index.rs
+++ runtime/src/in_mem_accounts_index.rs
@@ -1077,7 +1077,7 @@ impl<T: IndexValue> InMemAccountsIndex<T> {
     // return true if the removal was completed
     fn evict_from_cache(
         &self,
-        evictions: Vec<Pubkey>,
+        mut evictions: Vec<Pubkey>,
         current_age: Age,
         startup: bool,
         randomly_evicted: bool,
@@ -1091,7 +1091,20 @@ impl<T: IndexValue> InMemAccountsIndex<T> {
         if self.get_stop_evictions() {
             return false; // did NOT complete, ranges were changed, so have to restart
         }
+
+        // skip any keys that are held in memory because of ranges being held
         let ranges = self.cache_ranges_held.read().unwrap().clone();
+        if !ranges.is_empty() {
+            evictions.retain(|k| {
+                if ranges.iter().any(|range| range.contains(&k)) {
+                    // this item is held in mem by range, so don't remove
+                    completed_scan = false;
+                    false
+                } else {
+                    true
+                }
+            });
+        }
 
         let mut removed = 0;
         // consider chunking these so we don't hold the write lock too long
@@ -1112,12 +1125,6 @@ impl<T: IndexValue> InMemAccountsIndex<T> {
                     // marked dirty or bumped in age after we looked above
                     // these flushes will be handled in later passes (at later ages)
                     // but, at startup, everything is ready to age out if it isn't dirty
-                    continue;
-                }
-
-                if ranges.iter().any(|range| range.contains(&k)) {
-                    // this item is held in mem by range, so don't remove
-                    completed_scan = false;
                     continue;
                 }
 
