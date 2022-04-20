59e65b8444517cd4f166d25b524c87bcae371d29
Tyera Eulberg <tyera@solana.com>
2022-04-20 00:01:22 -0400

    Add program_id to TokenBalances structs (#24513)
    * Add program_id to TokenBalances structs
    
    * Update docs

--- ledger/src/blockstore.rs
+++ ledger/src/blockstore.rs
@@ -8781,6 +8781,7 @@ pub mod tests {
                     ui_amount_string: "1.1".to_string(),
                 },
                 owner: Pubkey::new_unique().to_string(),
+                program_id: Pubkey::new_unique().to_string(),
             }]),
             post_token_balances: Some(vec![TransactionTokenBalance {
                 account_index: 0,
@@ -8792,6 +8793,7 @@ pub mod tests {
                     ui_amount_string: "1.1".to_string(),
                 },
                 owner: Pubkey::new_unique().to_string(),
+                program_id: Pubkey::new_unique().to_string(),
             }]),
             rewards: Some(vec![Reward {
                 pubkey: "My11111111111111111111111111111111111111111".to_string(),
