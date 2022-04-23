4191147ebb81a6e354e782c6f62d5bc749b40007
Jeff Washington (jwash) <wash678@gmail.com>
2022-04-23 11:40:59 -0500

    > 0

--- runtime/src/expected_rent_collection.rs
+++ runtime/src/expected_rent_collection.rs
@@ -408,7 +408,7 @@ impl ExpectedRentCollection {
             2500 failed
             300 failed
             */
-        if partition_from_pubkey > 2 {
+        if partition_from_pubkey > 0 {
             return None;
         }
         
