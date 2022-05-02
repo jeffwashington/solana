920931a45a43c650cd063e242956f4dc5c7e4e1f
Jeff Washington (jwash) <wash678@gmail.com>
2022-05-02 12:56:12 -0500

    cleanup

--- runtime/src/expected_rent_collection.rs
+++ runtime/src/expected_rent_collection.rs
@@ -396,11 +396,11 @@ impl ExpectedRentCollection {
         find_unskipped_slot: impl Fn(Slot) -> Option<Slot>,
         filler_account_suffix: Option<&Pubkey>,
     ) -> Option<Hash> {
+            /*
         let slots_per_epoch = rent_collector
             .epoch_schedule
             .get_slots_in_epoch(rent_collector.epoch);
 
-            /*
         let partition_from_pubkey =
             crate::bank::Bank::partition_from_pubkey(pubkey, slots_per_epoch);
             /*
