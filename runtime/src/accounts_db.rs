78f8b770440641ee0593ecca586b3c147c929c6e
Jeff Washington (jwash) <wash678@gmail.com>
2022-05-16 14:42:01 -0500

    95

--- runtime/src/accounts_db.rs
+++ runtime/src/accounts_db.rs
@@ -6123,7 +6123,7 @@ impl AccountsDb {
                 if !bin_range.contains(&pubkey_to_bin_index) {
                     return;
                 }
-                if narrow.bin_from_pubkey(pubkey) <= nb * 91 / 100 {
+                if narrow.bin_from_pubkey(pubkey) <= nb * 95 / 100 {
                     return;
                 }
 
