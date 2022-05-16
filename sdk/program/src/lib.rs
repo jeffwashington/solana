752c0f4c5178e43d5d2377212838a88f2075e8ac
Jack May <jack@solana.com>
2022-05-16 12:30:37 -0700

    Quic client stats (#24195)
    * Add metrics to connection-cache to measure cache hits and misses
    
    * Add congestion stats
    
    * Add more client stats
    
    * Review comments
    
    Co-authored-by: Ryan Leung <ryan.leung@solana.com>

--- sdk/program/src/lib.rs
+++ sdk/program/src/lib.rs
@@ -780,6 +780,34 @@ macro_rules! unchecked_div_by_const {
     }};
 }
 
+use std::{mem::MaybeUninit, ptr::write_bytes};
+
+#[macro_export]
+macro_rules! copy_field {
+    ($ptr:expr, $self:ident, $field:ident) => {
+        std::ptr::addr_of_mut!((*$ptr).$field).write($self.$field)
+    };
+}
+
+pub fn clone_zeroed<T, F>(clone: F) -> T
+where
+    F: Fn(&mut MaybeUninit<T>),
+{
+    let mut value = MaybeUninit::<T>::uninit();
+    unsafe { write_bytes(&mut value, 0, 1) }
+    clone(&mut value);
+    unsafe { value.assume_init() }
+}
+
+// This module is purposefully listed after all other exports: because of an
+// interaction within rustdoc between the reexports inside this module of
+// `solana_program`'s top-level modules, and `solana_sdk`'s glob re-export of
+// `solana_program`'s top-level modules, if this module is not lexically last
+// rustdoc fails to generate documentation for the re-exports within
+// `solana_sdk`.
+#[cfg(not(target_arch = "bpf"))]
+pub mod example_mocks;
+
 #[cfg(test)]
 mod tests {
     use super::unchecked_div_by_const;
