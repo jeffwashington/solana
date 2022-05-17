d52b4ae41f6a6b893b669f151260f29f9ac0714e
Jeff Washington (jwash) <wash678@gmail.com>
2022-05-16 19:15:42 -0500

    panic

--- runtime/src/accounts_db.rs
+++ runtime/src/accounts_db.rs
@@ -5504,6 +5504,7 @@ impl AccountsDb {
         max_slot: Slot,
         config: &CalcAccountsHashConfig<'_>,
     ) -> Result<(Hash, u64), BankHashVerificationError> {
+        panic!("oops");
         return Ok((Hash::default(), 0));
         use BankHashVerificationError::*;
         let mut collect = Measure::start("collect");
