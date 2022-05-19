0882f402de4cb2561a96c7d89576207c3f45b13a
Jeff Washington (jwash) <wash678@gmail.com>
2022-05-19 15:08:19 -0500

    1 pass, log len of diffs

--- runtime/src/accounts_db.rs
+++ runtime/src/accounts_db.rs
@@ -111,7 +111,7 @@ pub const DEFAULT_NUM_DIRS: u32 = 4;
 // When calculating hashes, it is helpful to break the pubkeys found into bins based on the pubkey value.
 // More bins means smaller vectors to sort, copy, etc.
 pub const PUBKEY_BINS_FOR_CALCULATING_HASHES: usize = 65536;
-pub const NUM_SCAN_PASSES_DEFAULT: usize = 8;
+pub const NUM_SCAN_PASSES_DEFAULT: usize = 1;
 
 // Without chunks, we end up with 1 output vec for each outer snapshot storage.
 // This results in too many vectors to be efficient.
@@ -6255,7 +6255,7 @@ impl AccountsDb {
         let mut list2 = list2.write().unwrap();
         if !list2.is_empty() {
             list2.sort();
-            error!("jw4: {:?}", list2);
+            error!("jw4: {:?}", list2.len());
         }
 
         stats.sort_time_total_us += sort_time.load(Ordering::Relaxed);
