c7048c5aee51f34dfc85ce881ff920347e9a1b4e
Jeff Washington (jwash) <wash678@gmail.com>
2022-05-31 18:43:29 -0500

    more logs

--- runtime/src/accounts_db.rs
+++ runtime/src/accounts_db.rs
@@ -6607,6 +6607,7 @@ impl AccountsDb {
 
             self.calculate_accounts_hash_without_index(config, &storages, timings)
         } else {
+            error!("with index");
             self.calculate_accounts_hash(slot, config)
         }
     }
