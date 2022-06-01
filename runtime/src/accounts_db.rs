b359ae89d56b5551cdff718c1ec759b05fc70dee
Jeff Washington (jwash) <wash678@gmail.com>
2022-05-31 20:05:26 -0500

    remove noisy log

--- runtime/src/accounts_db.rs
+++ runtime/src/accounts_db.rs
@@ -7852,7 +7852,7 @@ impl AccountsDb {
                                         if !storages.is_empty() {
                                             Some((storages, *slot))
                                         } else {
-                                            error!("no accounts");
+                                            //error!("no accounts");
                                             None
                                         }
                                     },
