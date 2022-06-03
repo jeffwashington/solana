e984a43b2973b0de3203acb9275d0ef8b26ba859
behzad nouri <behzadnouri@gmail.com>
2022-06-03 10:36:27 -0400

    separates durable nonce and blockhash domains
    AdvanceNonceAccount instruction updates nonce to blockhash. This makes it
    possible that a durable transaction is executed twice both as a normal
    transaction and a nonce transaction if it uses blockhash (as opposed to nonce)
    for its recent_blockhash field.
    
    The commit prevents this double execution by separating nonce and blockhash
    domains; when advancing nonce account, blockhash is hashed with a fixed string.
    As a result a blockhash cannot be a valid nonce value; and if transaction was
    once executed as a normal transaction it cannot be re-executed as a durable
    transaction again and vice-versa.

--- rpc/src/rpc.rs
+++ rpc/src/rpc.rs
@@ -4421,7 +4421,8 @@ pub mod tests {
             hash::{hash, Hash},
             instruction::InstructionError,
             message::{v0, v0::MessageAddressTableLookup, MessageHeader, VersionedMessage},
-            nonce, rpc_port,
+            nonce::{self, state::DurableNonce},
+            rpc_port,
             signature::{Keypair, Signer},
             slot_hashes::SlotHashes,
             system_program, system_transaction,
@@ -5395,7 +5396,7 @@ pub mod tests {
                     42,
                     &nonce::state::Versions::new_current(nonce::State::new_initialized(
                         &authority,
-                        &Hash::default(),
+                        DurableNonce::default(),
                         1000,
                     )),
                     &system_program::id(),
