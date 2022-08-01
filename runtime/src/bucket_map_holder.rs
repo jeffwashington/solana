369e50bda654c8cb65764759b2f2b3f6848229c4
Jeff Washington (jwash) <wash678@gmail.com>
2022-08-01 14:47:11 -0500

    ce

--- runtime/src/bucket_map_holder.rs
+++ runtime/src/bucket_map_holder.rs
@@ -122,6 +122,7 @@ impl<T: IndexValue> BucketMapHolder<T> {
         loop {
             self.wait_dirty_or_aged
                 .wait_timeout(Duration::from_millis(self.age_interval_ms()));
+            use log::*;
             error!("{}{}", file!(), line!());
             if end_age == self.current_age() {
                 break;
