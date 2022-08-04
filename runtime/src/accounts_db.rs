117a4e429532e040c2393435d18cb3fbc16a83d8
Jeff Washington (jwash) <wash678@gmail.com>
2022-08-04 14:26:52 -0500

    don't log when there is no work to do for combining ancient slots

--- runtime/src/accounts_db.rs
+++ runtime/src/accounts_db.rs
@@ -3730,14 +3730,6 @@ impl AccountsDb {
         let mut current_ancient = None;
         let mut dropped_roots = vec![];
 
-        if let Some(first_slot) = sorted_slots.first() {
-            info!(
-                "ancient_append_vec: combine_ancient_slots first slot: {}, num_roots: {}",
-                first_slot,
-                sorted_slots.len()
-            );
-        }
-
         for slot in sorted_slots {
             let old_storages =
                 match self.get_storages_to_move_to_ancient_append_vec(slot, &mut current_ancient) {
@@ -3751,6 +3743,13 @@ impl AccountsDb {
             if guard.is_none() {
                 // we are now doing interesting work in squashing ancient
                 guard = Some(self.active_stats.activate(ActiveStatItem::SquashAncient))
+                if let Some(first_slot) = sorted_slots.first() {
+                    info!(
+                        "ancient_append_vec: combine_ancient_slots first slot: {}, num_roots: {}",
+                        first_slot,
+                        sorted_slots.len()
+                    );
+                }
             }
 
             // this code is copied from shrink. I would like to combine it into a helper function, but the borrow checker has defeated my efforts so far.
