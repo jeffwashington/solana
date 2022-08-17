6a478ffd7df4031cb3642d9ade196db55c2d91c0
Jeff Washington (jwash) <wash678@gmail.com>
2022-08-17 15:24:33 -0500

    add log

--- runtime/src/accounts_db.rs
+++ runtime/src/accounts_db.rs
@@ -6739,6 +6739,7 @@ impl AccountsDb {
         let check_hash = false;
         error!("jwash logging on update_accounts_hash_with_index_option, slot: {}", slot);
         if can_cached_slot_be_unflushed {
+            error!("forcing flush");
             self.flush_accounts_cache(true, Some(slot));
         }
         let (hash, total_lamports) = self
