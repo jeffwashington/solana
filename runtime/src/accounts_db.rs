26ec2018dc349a1b007cf99f3d354955c57e1901
Jeff Washington (jwash) <wash678@gmail.com>
2022-08-23 15:25:44 -0500

    log

--- runtime/src/accounts_db.rs
+++ runtime/src/accounts_db.rs
@@ -2502,6 +2502,7 @@ impl AccountsDb {
         // make sure these 3 special ones show up
         for special in [145377858, 145378948, 145380761] {
             if let Some(a) = self.get_storages_for_slot(145377858) {
+                error!("specially adding slot: {}", special);
                 dirty_stores.push((special, a.first().unwrap().clone()));
             }
         }
