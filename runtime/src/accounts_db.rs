30d9db47f0aa68046440e232f78a8004b89b211e
jeff washington <jeff.washington@solana.com>
2022-09-06 16:52:54 -0500

    assert

--- runtime/src/accounts_db.rs
+++ runtime/src/accounts_db.rs
@@ -5245,7 +5245,7 @@ if stores.is_some() {
         let mut write = slot_storages
         .write()
         .unwrap();
-        //assert!(write.len() < 3, "{}", write.len());
+        assert!(write.len() < 10, "{}", write.len());
         if write.len() > 10 {
             error!("inserting into: {}", write.len());
         }
