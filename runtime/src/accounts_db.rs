4838e8f9ab6dd6f65971c0f9c6b70cf37511c927
jeff washington <jeff.washington@solana.com>
2022-09-14 10:28:02 -0500

    new path

--- runtime/src/accounts_db.rs
+++ runtime/src/accounts_db.rs
@@ -8308,8 +8308,8 @@ impl AccountsDb {
         genesis_config: &GenesisConfig,
     ) -> IndexGenerationInfo {
 
-        let p1 = Path::new("/mnt/nvme1n1/success_150337130");
-        let p2 = Path::new("/mnt/nvme1n1/failed_150337130");
+        let p1 = Path::new("/mnt/nvme1n1/success_150353655");
+        let p2 = Path::new("/mnt/nvme1n1/150353655");
         CacheHashData::compare_two(&[&p1, &p2]);
         use log::*;error!("{}{}", file!(), line!());
 
