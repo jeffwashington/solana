a51dfd7c88c767811db09b51c4479f0b15446719
jeff washington <jeff.washington@solana.com>
2022-10-07 10:53:09 -0500

    log before panic

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -8007,6 +8007,7 @@ pub struct TotalAccountsStats {
 
 impl Drop for Bank {
     fn drop(&mut self) {
+        error!("bank drop: {}", self.slot());
         panic!("bank drop: {}", self.slot());
         if self.rc.accounts.accounts_db.duplicates.contains(&self.slot()) {
             panic!("bank drop: {}", self.slot());
