0152447c4ceae073322c1267dda62fa189a47e60
jeff washington <jeff.washington@solana.com>
2022-11-03 08:20:04 -0500

    kin nov 4 2022

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -5351,6 +5351,10 @@ impl Bank {
     }
 
     fn collect_rent_eagerly(&self, just_rewrites: bool) {
+        if !just_rewrites || self.slot() > 99273 {
+            // skip rent collection for all but the first bank for kin
+            return;
+        }
         if self.lazy_rent_collection.load(Relaxed) {
             return;
         }
@@ -7129,7 +7133,7 @@ impl Bank {
                 "Capitalization mismatch: calculated: {} != expected: {}",
                 calculated, expected
             );
-            false
+            true
         }
     }
 
