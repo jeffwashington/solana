02b79cbaf8005519fe7bc81217fb39cfbd098580
jeff washington <jeff.washington@solana.com>
2023-07-17 16:41:59 -0500

    skip startup hash verification

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -7197,6 +7197,8 @@ impl Bank {
                         info!(
                             "running initial verification accounts hash calculation in background"
                         );
+                        let result = true;
+                        /*
                         let result = accounts_.verify_accounts_hash_and_lamports(
                             slot,
                             cap,
@@ -7212,6 +7214,7 @@ impl Bank {
                                 include_slot_in_hash,
                             },
                         );
+                        */
                         accounts_
                             .accounts_db
                             .verify_accounts_hash_in_bg
