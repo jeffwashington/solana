3eaef14d956682a89d6d7c264e608e1549b6f046
jeff washington <jeff.washington@solana.com>
2023-07-17 16:10:03 -0500

    elimnate massive log

--- runtime/src/accounts_hash.rs
+++ runtime/src/accounts_hash.rs
@@ -1054,7 +1054,7 @@ impl AccountsHasher {
             None,
         );
         hash_time.stop();
-        error!("jwash: hash final: {}, {:?}", hash_time.as_us(), cumulative.cumulative);
+        error!("jwash: hash final: {}, {:?}", hash_time.as_us(), ());// this is massive logging: cumulative.cumulative);
         stats.hash_time_total_us += hash_time.as_us();
         (hash, total_lamports)
     }
