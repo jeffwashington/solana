6bdaf91c163690464957e13d0e229fc956776b7b
jeff washington <jeff.washington@solana.com>
2023-08-11 19:36:41 -0500

    track down under allocate

--- accounts-db/src/accounts_db.rs
+++ accounts-db/src/accounts_db.rs
@@ -2409,6 +2409,7 @@ struct ScanState<'a> {
     sort_time: Arc<AtomicU64>,
     cache_data: Option<CacheHashDataFile>,
     db: &'a AccountsDb,
+    count: usize,
 }
 
 impl<'a> Clone for ScanState<'a> {
@@ -2423,6 +2424,7 @@ impl<'a> Clone for ScanState<'a> {
             // this is the only non-trivial clone
             cache_data: None,
             db: self.db,
+            count: 0,
         }
     }
 }
@@ -2465,10 +2467,18 @@ impl<'a> AppendVecScan for ScanState<'a> {
             }
         }
         let source_item = CalculateHashIntermediate::new(loaded_hash, balance, *pubkey);
-        self.cache_data
-            .as_mut()
-            .unwrap()
-            .get_slice_mut(self.i as u64)[0] = source_item;
+        let elts = self.cache_data.as_ref().map(|cache_data| cache_data.get_num_elts()).unwrap_or_default();
+        if elts <= self.i {
+            //log::error!("too few elts: {}, {}, {}, {}", elts, self.i, self.count, self.cache_data.is_none());
+        }
+        let m = self.cache_data
+        .as_mut()
+        .unwrap()
+        .get_slice_mut(self.i as u64);
+        if m.len() == 0 {
+            log::error!("too few elts: {}, {}, {}", elts, self.i, self.count);
+        }
+        m[0] = source_item;
         self.i += 1;
     }
     fn scanning_complete(self) -> Option<CacheHashDataFile> {
@@ -7273,10 +7283,18 @@ impl AccountsDb {
     where
         S: AppendVecScan,
     {
+        let count = storage.count();
+        let mut found = 0;
         storage
             .accounts
             .account_iter()
-            .for_each(|account| scanner.found_account(&LoadedAccount::Stored(account)));
+            .for_each(|account| {
+                scanner.found_account(&LoadedAccount::Stored(account));
+                found += 1;
+            });
+        if found > count {
+            error!("storage expected {}, had {}", count, found);
+        }
     }
 
     fn update_old_slot_stats(&self, stats: &HashStats, storage: Option<&Arc<AccountStorageEntry>>) {
@@ -7772,6 +7790,7 @@ impl AccountsDb {
             sort_time: sort_time.clone(),
             db: self,
             cache_data: None,
+            count: 0,
         };
 
         let result = self.scan_account_storage_no_bank(
