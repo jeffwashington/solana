7ce4f807b321243ad77cbc616abef5ca9bd10525
brooks <brooks@solana.com>
2023-10-03 16:45:29 -0400

    wip: sleep for 1 second in flush-loop

--- accounts-db/src/accounts_db.rs
+++ accounts-db/src/accounts_db.rs
@@ -4454,7 +4454,7 @@ impl AccountsDb {
                     self.flush_accounts_cache(false, Some(flush_root));
                 }
 
-                std::thread::sleep(Duration::from_secs(5));
+                std::thread::sleep(Duration::from_secs(1));
             },
         );
         info!(
