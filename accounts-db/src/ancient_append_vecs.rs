a9c70dede20e8e632dd7a2ca2a2ab15b54d52fb1
jeff washington <jeff.washington@solana.com>
2023-10-13 17:08:49 -0500

    always log ancient shrink stats

--- accounts-db/src/ancient_append_vecs.rs
+++ accounts-db/src/ancient_append_vecs.rs
@@ -276,7 +276,7 @@ impl AccountsDb {
 
         // only log when we've spent 1s total
         // results will continue to accumulate otherwise
-        if self.shrink_ancient_stats.total_us.load(Ordering::Relaxed) > 1_000_000 {
+        {//if self.shrink_ancient_stats.total_us.load(Ordering::Relaxed) > 1_000_000 {
             self.shrink_ancient_stats.report();
         }
     }
