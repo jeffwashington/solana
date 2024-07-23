b69e806daaa3b3fd1aefff798ab81c946b19c32a
jeff washington <jeff.washington@solana.com>
2024-07-23 15:26:15 -0500

    rename to remove `excluded`

--- accounts-db/src/accounts_hash.rs
+++ accounts-db/src/accounts_hash.rs
@@ -205,7 +205,7 @@ pub struct HashStats {
     pub sum_ancient_scans_us: AtomicU64,
     pub count_ancient_scans: AtomicU64,
     pub pubkey_bin_search_us: AtomicU64,
-    pub num_excluded_zero_lamport_accounts: AtomicU64,
+    pub num_zero_lamport_accounts: AtomicU64,
 }
 impl HashStats {
     pub fn calc_storage_size_quartiles(&mut self, storages: &[Arc<AccountStorageEntry>]) {
@@ -308,9 +308,8 @@ impl HashStats {
                 i64
             ),
             (
-                "num_excluded_zero_lamport_accounts",
-                self.num_excluded_zero_lamport_accounts
-                    .load(Ordering::Relaxed),
+                "num_zero_lamport_accounts",
+                self.num_zero_lamport_accounts.load(Ordering::Relaxed),
                 i64
             ),
         );
@@ -1174,7 +1173,7 @@ impl<'a> AccountsHasher<'a> {
                 hashes.write(&item.hash.0);
             } else {
                 stats
-                    .num_excluded_zero_lamport_accounts
+                    .num_zero_lamport_accounts
                     .fetch_add(1, Ordering::Relaxed);
                 // if lamports == 0, check if they should be included
                 if self.zero_lamport_accounts == ZeroLamportAccounts::Included {
